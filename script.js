3362697e42138e3ff56bd90ce246d458b53a053e
hyojung507 <hkim27@risd.edu>
2015-03-31 16:08:05 -0400

    f

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  $(function() {
+    $( "#image1" ).draggable();
+  });
+
+
+  $(function() {
+    $( "#image2" ).draggable();
+  });
+
+    $(function() {
+    $( "#image3" ).draggable();
+  });
